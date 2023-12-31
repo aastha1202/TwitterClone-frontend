195c7d20fabe3dbb9cc087bbcd92e75e13296213
Aastha Singh <saastha2002@gmail.com>
2024-01-01 00:17:54 +0530

    removed undefined

--- clients/api.ts
+++ clients/api.ts
@@ -1,7 +1,5 @@
 import { GraphQLClient } from "graphql-request";
 
-// const isClient = typeof window !=="undefined"
-
 console.log(typeof window)
 export const graphQlClient= new GraphQLClient('https://twitterclone-backend-jx9v.onrender.com/graphql/',{
     headers:()=>({
